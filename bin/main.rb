f15396f75d31fdcbfa2785c8676731569d1374df
lucasmazo32 <lucasmazo32@gmail.com>
2019-12-27 17:24:28 -0500

    Everything is working as expected with the game so far. We need to add an stoping condition for the game when someone wins

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,40 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-# options for movement
+class Player
+  attr_reader :name
+  attr_reader :sym
+
+  def initialize(sym, name)
+    @sym = sym
+    @name = name
+  end
+end
 
 class Board
+
   def initialize
     @state = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
     show # try
   end
 
+  def available
+    @state.each_with_index{ |x, index| print "#{index + 1} " if x == ' ' }
+  end
+
   def position(num_pos, sym)
-    @state[num_pos] = sym
+    check = true
+    move = num_pos.to_i
+    while check
+      if move <= 9 && move >= 1 && (move.is_a? Integer) && @state[move- 1] == ' '
+        @state[move -1] = sym
+        check = false
+      else
+        puts "Incorrect value, please type a correct one."
+        puts "Correct numbers are #{available}"
+        move = gets.chomp.to_i
+      end
+    end
     show
   end
 
@@ -24,15 +48,16 @@ class Board
   end
 end
 
-class Player
-  def initialize(sym)
-    @sym = sym
-  end
-  
-end
+puts "Player 1, type your name"
+player1name = gets.chomp
 
+
+puts "Player 2, type your name"
+player2name = gets.chomp
+
+player1 = Player.new('X', player1name)
+player2 = Player.new('O', player2name)
 main_board = Board.new
-player1 = Player.new('X')
 
 # variables for win condition
 draw = true
@@ -40,14 +65,17 @@ counter = 0
 
 # actual game loop
 while draw && counter < 9
-  state(square_spaces)
+  main_board.available
   if counter.even?
-    puts ' ' # mechanics for player 1 | puts to evade rubocop
+    puts "Is your turn #{player1.name}, select an empty space"
+    election = gets.chomp
+    main_board.position(election, player1.sym)
   else
-    puts '' # mechanics for plater 2 | puts to evade rubocop
+    puts "Is your turn #{player2.name}, select an empty space"
+    election = gets.chomp
+    main_board.position(election, player2.sym)
   end
   counter += 1
-  # wait = gets # gets for now so that it doesn't just loop 9 times
 end
 
 if draw
